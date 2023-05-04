74a2ca355246b9736c5cb3a8458125dc74be0c4c
ShubhamSukum <shubhamsukum1722002@gmail.com>
2023-05-04 11:21:41 +0530

    04/05/2023 1st

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,6 +3,7 @@ import Axios from "axios";
 
 export const Home=()=>{
     const [category,allCatergory]=useState([]);
+    const [testi,allTesti]=useState([]);
 
     useEffect(()=>{
         Axios.get("http://localhost:3001/categories").then((res)=>{
@@ -10,19 +11,43 @@ export const Home=()=>{
         })
     },[]);
 
+    useEffect(()=>{
+        Axios.get("http://localhost:3001/getTesti").then((res)=>{
+            allTesti(res.data);
+        })
+    },[]);
+
     return(
     <div>
-        {
-            category.map((cat)=>{
-                return(
-                    <div>
+        <div className="catogories-css">
+            {
+                category.map((cat)=>{
+                    return(
                         <div>
-                            <img src={cat.imageURL} alt={cat.compName}/>
+                            <div>
+                                <img src={cat.imageURL} alt={cat.compName}/>
+                            </div>
+                            <p>{cat.compName}</p>
                         </div>
-                        <p>{cat.compName}</p>
-                    </div>
-                )
-            })
-        }
+                    )
+                })
+            }
+        </div>
+
+        <p style={{fontSize:"2.5vh",fontWeight:"bolder"}}>TESTIMONIALS</p>
+
+        <div className="testi-css">
+            {
+                testi.map((t)=>{
+                    return(
+                        <div>
+                            <p><strong>{t.judge}</strong></p>
+                            <p>{t.words}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+
     </div>)
 };
