1e913489c7388f359dcf76ec36600aeece633666
Onkar Swami <onkarswami2002@gmail.com>
2023-05-07 01:18:06 +0530

    Navbar styling done along with some styling changes in login & signup pages

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,41 +1,54 @@
-import {Link,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import '../App.css';
 
-export const Navbar=()=>{
-    const [cookies,setCookies]=useCookies(["access_token"]);
+export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
     // console.log(cookies["access_token"]);
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
-    const logout=()=>{
-        setCookies("access_token","");
-        window.localStorage.removeItem("userId");   
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userId");
         console.log("Executed!!")
         navigate("/login")
     };
-    
-    return(
-    <div className="navbar">
-        <Link to="/" className="link"> Home </Link>
-        <Link to="/courses" className="link">Courses</Link>
 
-        {
-            !cookies["access_token"]?
-            (
-                <div>
-                    <Link to="/signup" className="link"> Sign Up </Link>
-                    <Link to="/login" className="link"> Login </Link>
-                </div>
-            ):
-            (<button onClick={logout} className="logout-btn">Logout</button>)
-        }
+    return (
+        <>
+            <nav>
+                <input type="checkbox" id="check" />
+                <label for="check" className="checkbtn">
+                    <i className="fa fa-bars"></i>
+                </label>
+                <label className="logo">EaseE-Learning</label>
+                <ul>
+                    <li><Link to="/" className="link"> Home </Link></li>
+                    <li><Link to="/courses" className="link">Courses</Link></li>
+                </ul>
 
-        {/* Comments */}
+                {
+                    !cookies["access_token"] ?
+                        (
+                            <ul>
+                                <li><Link to="/signup" className="link"> Sign Up </Link></li>
+                                <li><Link to="/login" className="link"> Login </Link></li>
+                            </ul>
+                        ) :
+                        (
+                            <ul>
+                                <li><button onClick={logout} className="logout-btn">Logout</button></li>
+                            </ul>
+                        )
+                }
 
-        {/* <Link to="/signup" className="link"> Sign Up </Link>
+                {/* Comments */}
+
+                {/* <Link to="/signup" className="link"> Sign Up </Link>
             <Link to="/login" className="link"> Login </Link> */}
 
-        {/* {
+                {/* {
             !cookies["access_token"]?
             (
                 <div>
@@ -49,7 +62,8 @@ export const Navbar=()=>{
                 </div>
             )
         } */}
-    </div>
+            </nav>
+        </>
     )
 };
 
