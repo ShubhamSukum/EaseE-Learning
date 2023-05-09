f58302b3ef5f28639e5f11ea3b7cd82f9b506b2f
Onkar Swami <onkarswami2002@gmail.com>
2023-05-09 19:16:53 +0530

    Added styling to courseYT.js

--- client/src/pages/courseYT.js
+++ client/src/pages/courseYT.js
@@ -33,13 +33,13 @@
 //     //     axios.post("http://localhost:3001/displayvideo",{courseId:params.ytcourse,moduleNo:clicked["moduleNo"]})
 //     //     .then((res)=>{
 //     //         setVideo([res.data]);
-            
+
 //     //     }).catch((err)=>{
 //     //         console.log(err);
 //     //     })
 //     // });
 
-    
+
 //     // useEffect(()=>{
 //     //     axios.post("http://localhost:3001/displayvideo",{courseId:params.ytcourse,moduleNo})
 //     // },[params.ytcourse,moduleNo]);
@@ -47,21 +47,21 @@
 
 
 //     return(
-        
+
 //         <>
 //         <div>{}</div>
 //             {
 //                 ytCourse.map((info,index)=>{
-                    
+
 //                     return(
-                        
+
 //                         <div key={index} >
 //                                 <p>Topic Name == {info.topicName}</p> 
 //                                 {/* <p>Instructor Name == {info.instructor}</p>
 //                                 <p>Instructor Info == {info.instructorInfo}</p> */}
 //                                 <img src={info.instructorDp} style={{width:"10vw",height:"10vw"}}/>
 //                                 {/* <p>Enrolled == {info.enrolled}</p> */}
-                                            
+
 //                         </div>
 //                     )
 //                 })
@@ -69,8 +69,8 @@
 
 //             {/* modules */}
 
-            
-            
+
+
 
 //             {
 //                 ytCourse.map((info,index)=>{
@@ -81,17 +81,17 @@
 //                             info.Module.map((i,index)=>{
 //                                 return(<>
 //                                     <div className="sad">
-                                        
+
 //                                     </div>
 //                                 </>)
 //                                 })
 //                         } */}
-                            
+
 
 //                             <div key={index}>
 //                                 {
-                                    
-                                
+
+
 //                                     info.Module.map((i,index)=>{
 //                                         // const clicked=(link)=>{
 //                                         //     try{
@@ -100,7 +100,7 @@
 //                                         //     }catch(err){
 //                                         //         console.log(err);
 //                                         //     }
-                                            
+
 //                                         // }
 
 //                                         // const funClick=async()=>{
@@ -116,7 +116,7 @@
 //                                                     onClicked(i);
 //                                                     console.log(clicked.moduleNo);
 //                                                     console.log(clicked.moduleLink);
-                                                    
+
 //                                                     // console.log(clicked);
 //                                                     // console.log(modu);
 //                                                 }}
@@ -135,7 +135,7 @@
 //                 })
 //             }
 
-            
+
 //         </>
 //     )
 // };
@@ -145,93 +145,99 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export const YtCourse = () => {
-  const [ytCourse, setYtCourse] = useState([]);
-  const [, onClicked] = useState({});
-  const [video, setVideo] = useState("");
+    const [ytCourse, setYtCourse] = useState([]);
+    const [, onClicked] = useState({});
+    const [video, setVideo] = useState("");
 
-  const params = useParams();
+    const params = useParams();
 
-  useEffect(() => {
-    axios
-      .post("http://localhost:3001/moduleYt", { courseId: params.ytcourse })
-      .then((res) => {
-        setYtCourse(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [params.ytcourse]);
+    useEffect(() => {
+        axios
+            .post("http://localhost:3001/moduleYt", { courseId: params.ytcourse })
+            .then((res) => {
+                setYtCourse(res.data);
+                setVideo(res?.data[0]?.Module[0]?.moduleLink);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [params.ytcourse]);
 
-//   useEffect(() => {
-//     if (clicked.moduleNo) {
-//       axios
-//         .post("http://localhost:3001/displayvideo", {
-//           courseId: params.ytcourse,
-//           moduleNo: clicked.moduleNo,
-//         })
-//         .then((res) => {
-//           setVideo(res.data);
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     }
-//   }, [clicked.moduleNo, params.ytcourse]);
+    //   useEffect(() => {
+    //     if (clicked.moduleNo) {
+    //       axios
+    //         .post("http://localhost:3001/displayvideo", {
+    //           courseId: params.ytcourse,
+    //           moduleNo: clicked.moduleNo,
+    //         })
+    //         .then((res) => {
+    //           setVideo(res.data);
+    //         })
+    //         .catch((err) => {
+    //           console.log(err);
+    //         });
+    //     }
+    //   }, [clicked.moduleNo, params.ytcourse]);
 
-  return (
-    <>
-     <div>
-      <iframe
-        title="video"
-        src={video}
-        width="560"
-        height="315"
-        frameborder="0"
-        allowfullscreen
-      ></iframe>
-    </div>
-      {ytCourse.map((info, index) => {
-        return (
-          <div key={index}>
-            <p>Topic Name == {info.topicName}</p>
-            <img
-              src={info.instructorDp}
-              style={{ width: "10vw", height: "10vw" }}
-              alt={info.instructor}
-            />
-          </div>
-        );
-      })}
+    return (
+        <>
+            <div class="main">
+                <div class="right">
+                    <div class="videos">
+                        <iframe title="video" src={video} frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    
+                    <hr />
 
-      {ytCourse.map((info, index) => {
-        return (
-          <div key={index}>
-            <div key={index}>
-              {info.Module.map((i, index) => {
-                return (
-                  <div
-                    key={index}
-                    style={{
-                      marginTop: "10vh",
-                      border: "1px solid black",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => {
-                      onClicked(i);
-                      setVideo(i.moduleLink);
-                    }}
-                  >
-                    <h2>Topic Name ** {info.topicName}</h2>
-                    <h5>Module No == {i.moduleNo}</h5>
-                    <h5>Module Name == {i.moduleName}</h5>
-                  </div>
-                );
-              })}
+                    {ytCourse.map((info, index) => {
+                        return (
+                            <div class="instructor">
+                                <div style={{ width: "50%" }} key={index}>
+
+                                    <img
+                                        src={info.instructorDp}
+                                        style={{ width: "10vw", height: "10vw" }}
+                                        alt={info.instructor}
+                                    />
+
+                                </div>
+                                <div class="instructor-info">
+                                    <h2>{info.topicName}</h2>
+                                    
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+
+                <div className="left" >
+                    <h1>  {ytCourse[0]?.topicName}</h1>
+                    <div className="modulie">
+                        {ytCourse.map((info, index) => {
+                            return (
+                                info.Module.map((i, index) => {
+                                    return (
+                                        <ul>
+                                            <li onClick={() => {
+                                                onClicked(i);
+                                                setVideo(i.moduleLink);
+                                            }}
+                                            >
+                                                <p>
+                                                    <span> <i class="fa fa-youtube-play" aria-hidden="true"></i> </span>
+                                                    {i.moduleNo}. {i.moduleName}
+                                                </p>
+                                            </li>
+                                            <hr />
+                                        </ul>
+                                    );
+                                })
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
-          </div>
-        );
-      })}
-    </>
-  );
+        </>
+    );
 };
 
