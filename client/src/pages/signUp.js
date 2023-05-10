d6e658ca20493e67e140eccbaa228f0dd0c35014
Onkar Swami <onkarswami2002@gmail.com>
2023-05-10 19:08:27 +0530

    Added validation for Password and Confirm Password

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -42,8 +42,159 @@ export const SignUp = () => {
         }
     };
 
+    function validatePassword() {
+        var pass = document.getElementById('password').value;
+        var passDiv = document.getElementById('passDiv');
+        passDiv.style.display = "block";
+        clear();
+        if (pass === "") {
+            passDiv.style.display = "none";
+            document.getElementById("passNumber").style.color = "red";
+            document.getElementById("passLetter").style.color = "red";
+            document.getElementById("passSpecial").style.color = "red";
+            document.getElementById("passLength").style.color = "red";
+        }
+        test1(pass);
+        test2(pass);
+        test3(pass);
+        test4(pass);
+
+        if (test1(pass) && test2(pass) && test3(pass) && test4(pass)) {
+            document.getElementById("cpassError").innerHTML = "Password is Strong";
+            document.getElementById("cpassError").style.color = "limegreen";
+            document.getElementById("cpassError").style.visibility = "Visible";
+            document.getElementById("password").style.borderColor = "2.3px solid black";
+            passDiv.classList.add("animate-hide");
+            document.getElementById('goUP').classList.add("go-up");
+        } else {
+            document.getElementById("cpassError").innerHTML = "Password is Week";
+            document.getElementById("cpassError").style.color = "red";
+            document.getElementById("cpassError").style.visibility = "Visible";
+            passDiv.classList.remove("animate-hide");
+            document.getElementById('goUP').classList.remove("go-up");
+        }
+    }
+
+    function confirmPassword() {
+        var pass = document.getElementById('password').value;
+        var cpass = document.getElementById('cpassword').value;
+
+        if (cpass === "") {
+            document.getElementById("cpassError").style.color = "red";
+            document.getElementById("cpassError").innerHTML = "Passwords are not matching";
+            return false;
+        }
+
+        if (pass === cpass) {
+            if (test1(cpass) && test2(cpass) && test3(cpass) && test4(cpass)) {
+                document.getElementById("password").style.borderBottom = "2.3px solid black";
+                document.getElementById("cpassword").style = "border-color: solid black !important";
+                document.getElementById("cpassError").innerHTML = "Passwords are matched";
+                document.getElementById("cpassError").style.visibility = "Visible";
+                document.getElementById("cpassError").style.color = "limegreen";
+                return true;
+            } else {
+                document.getElementById("cpassError").innerHTML =
+                    "Password is too Week, follow above instructions to create your password strong";
+                document.getElementById("cpassError").style.visibility = "Visible";
+                document.getElementById("cpassError").style.color = "red";
+                return false;
+            }
+        } else {
+            document.getElementById("cpassword").style.borderBottomColor = "red";
+            document.getElementById("cpassError").innerHTML = "Passwords are not matching";
+            document.getElementById("cpassError").style.visibility = "Visible";
+            document.getElementById("cpassError").style.color = "red";
+
+            return false;
+        }
+    }
+
+    function test1(pass) {
+
+        if (pass.search(/[0-9]/) === -1) {
+            document.getElementById("password").style = "border-color: red !important";
+            document.getElementById("passNumber").style.color = "red";
+            return false;
+        } else {
+            document.getElementById("passNumber").style.color = "limegreen";
+            document.getElementById("password").style = "border-color: solid black !important";
+            return true;
+        }
+    }
+
+    function test2(pass) {
+
+        if (pass.search(/[A-Z]/) === -1) {
+            document.getElementById("password").style = "border-color: red !important";
+            document.getElementById("passLetter").style.color = "red";
+            return false;
+        } else {
+            document.getElementById("passLetter").style.color = "limegreen";
+            document.getElementById("password").style = "border-color: solid black !important";
+            return true;
+        }
+    }
+
+    function test3(pass) {
+
+        if (pass.search(/[!@#%^&(_+,;:]/) === -1) {
+            document.getElementById("password").style = "border-color: red !important";
+            document.getElementById("passSpecial").style.color = "red";
+            return false;
+        } else {
+            document.getElementById("passSpecial").style.color = "limegreen";
+            document.getElementById("password").style = "border-color: solid black !important";
+            return true;
+        }
+    }
+
+    function test4(pass) {
+
+        if (pass.length < 8) {
+            document.getElementById("password").style = "border-color: red !important";
+            document.getElementById("passLength").style.color = "red";
+            return false;
+        } else {
+            document.getElementById("passLength").style.color = "limegreen";
+            document.getElementById("password").style = "border-color: solid black !important";
+            return true;
+        }
+    }
+
+    function clear() {
+        var cpasss = document.getElementById('cpassword');
+        cpasss.value = "";
+    }
+
     return (
-        <div className="center">
+        <>
+
+<center id="passDiv" style={{display:"none"}}>
+        <fieldset style={{width: "95%"}}>
+            <ul style={{width: "fit-content", margin:"0 auto"}}>
+
+                <li>
+                    <div style={{textAlign: "left"}} id="passNumber">Password should contain at-least 1 number</div>
+                </li>
+                <li>
+                    <div style={{textAlign: "left"}} id="passLetter">Password should contain at-least one uppercase letter
+                    </div>
+                </li>
+                <li>
+                    <div style={{textAlign: "left"}} id="passSpecial">Password should contain at-least 1 special character
+                    </div>
+                </li>
+                <li>
+                    <div style={{textAlign: "left"}} id="passLength">Length of password should be minimum 8 characters
+                    </div>
+                </li>
+
+            </ul>
+        </fieldset>
+    </center>
+
+        <div className="center" id="goUP">
             <h1>Sign UP</h1>
             <i class="fa fa-user-circle" aria-hidden="true" style={{ paddingBottom: "15px", fontSize: "40px" }}></i>
             <form onSubmit={onSubmit}>
@@ -61,15 +212,16 @@ export const SignUp = () => {
 
                 <div className="form-group">
                     {/* <label htmlFor="Password">Password</label> */}
-                    <input type="password" placeholder="Enter Password"
-                        value={password} onChange={(event) => setpassword(event.target.value)} />
+                    <input type="password" placeholder="Enter Password" name="password" id="password"
+                        value={password} onKeyUp={validatePassword} onChange={(event) => setpassword(event.target.value)} />
                 </div>
 
                 <div className="form-group">
                     {/* <label htmlFor="Confirm Password">Confirm Password</label> */}
-                    <input type="password" placeholder="Confirm Password"
-                        value={confirmPass} onChange={(event) => setconfirmPass(event.target.value)} />
+                    <input type="password" placeholder="Confirm Password" name="cpassword" id="cpassword"
+                        value={confirmPass} onKeyUp={confirmPassword} onChange={(event) => setconfirmPass(event.target.value)} />
                 </div>
+                <small id="cpassError" style={{color: "red",visibility: "hidden"}}></small>
 
                 <div className="form-group">
                     {/* <label htmlFor="College">College</label> */}
@@ -86,8 +238,8 @@ export const SignUp = () => {
                 <button type="submit" id="signUP-Button">Sign Up</button>
                 <br />
                 <p className="Login-direct">Already Have an Account: </p>
-                <Link to="/login" className="Login-direct">Login</Link>
+                <p><Link to="/login" className="Login-direct">Login</Link></p>
             </form>
-        </div>
+        </div></>
     )
 };
